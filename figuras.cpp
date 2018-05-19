0f319bcec0f9cb81b08d06acad616d47f1653c04
Alejandro <alejandro.redondo.ayala@gmail.com>
2018-05-19 22:03:29 +0200

    Actualizacion figuras
    Subo lo que llevo hecho de figuras NO es el definitivo pero para que quede guardado

--- figuras.cpp
+++ figuras.cpp
@@ -3,20 +3,20 @@
  #include<GL/glut.h>
 
     float color[3]={1.0,0.545,0.0};
-        float position[6]={-0.144,-0.190,0.0,0.0,0.0,0.0};
+        float position[6]={-0.180,-0.205,0.0,0.0,0.0,0.0};
   Minero::Minero(){
     radio=0.2;
     coneHeight=0.3;
   
   }
   
-  void Minero:: drawMinero(){
+  void Minero:: drawMinero(float x,float y){
           
      //dibujo el cuerpo del minero
      
      glPushMatrix();
      //situar posicion para dibujar
-     glTranslatef(position[0],position[1],position[2]);
+     glTranslatef(x,y,position[2]);
      glRotatef(position[3],1,0,0);
      glRotatef(position[4],0,1,0);
      glRotatef(position[5],0,0,1);
@@ -25,19 +25,54 @@
      //dibujo del cuerpo
      
      glutSolidCone(radio,coneHeight,20,20);
-     
+     glPopMatrix();
   
      
      //dibujo de la cabeza
-     
-     glTranslatef(position[0],position[1],position[2]+coneHeight+(radio/2));
-     glColor3f(1.0,0,0);
+     glPushMatrix();
+     glTranslatef(x,y,position[2]+coneHeight+(radio/2));
+     glColor4f(0.0F,1.0F,0.0F,1.0F);
      //no cambio la rotación pues es la misma
-     glutSolidSphere(radio,10,10);
+     glutSolidSphere(radio,20,20);
      
      glPopMatrix();
       
    
 }
+
+  Cuadro::Cuadro(){
+     x=0;
+     y=0;    }
+    
+  void Cuadro:: drawCuadro(int x, int y, Colors col){
+  
+     glDisable(GL_LIGHTING);
+     glPushMatrix();
+     switch(col){
+        case Colors::RED:
+         glColor4f(1.0f,0.0f,0.0f,0.5f);
+         break;
+         case Colors::GREEN:
+         glColor4f(0.0f,1.0f,0.0f,0.5f);
+         break;
+         case Colors::BLUE:
+         glColor4f(0.0f,0.0f,1.0f,0.5f);
+         break;
+     
+     
+     }
+       
+    glBegin(GL_POLYGON);
+    
+    glVertex3f(0.035f+(y-11.0)*0.422, 0.0f+(11.0-x)*0.422,0.01);
+    glVertex3f(0.035f+(y-11.0)*0.422, -0.415f+(11.0-x)*0.422,0.01);
+    glVertex3f(-0.380f+(y-11.0)*0.422, -0.415f+(11.0-x)*0.422,0.01);
+    glVertex3f(-0.380f+(y-11.0)*0.422, 0.0f +(11.0-x)*0.422,0.01);
+    glEnd();
+    glPopMatrix();
+    glEnable(GL_LIGHTING);
+  
+  }
+   
   
   
--- figuras.hpp
+++ figuras.hpp
@@ -5,7 +5,25 @@
         float coneHeight;
      public:
         Minero();
-        void drawMinero();
-        //setPos();
+        void drawMinero(float x, float y);
+        //void setPos(float x. float y);
+        
+  };
+  
+  enum class Colors{
+     RED, GREEN , BLUE};
+     
+     
+  class Cuadro{
+     public:
+        Cuadro();
+        void drawCuadro(int x, int y, Colors col);
+        int x;
+        int y;
+     
+     
+     
+     
+  
   
   };
